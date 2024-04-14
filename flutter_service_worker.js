bd698be7ef823dce19961ca82bf0e144be71129b
mu-projects <m.sgrgl@hotmail.com>
2024-04-14 17:40:37 +0300

    _

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8cf97a908bfff3945a7fb09799d1564e",
-"/": "8cf97a908bfff3945a7fb09799d1564e",
+"index.html": "fdab733c0a9839896a538b0bcc322e49",
+"/": "fdab733c0a9839896a538b0bcc322e49",
 "main.dart.js": "5938a398d73cd8857b35b8f1dccf7872",
 "manifest.json": "484bcfaee7dda26c8934b6f0a1f7c1c1",
 "version.json": "b3c25d78bb96f4352f7edc20a377d58b"};
