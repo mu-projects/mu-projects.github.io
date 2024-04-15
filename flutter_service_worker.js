6741654b58435fe82e1c51f57a34661a2cd8868a
mu-projects <m.sgrgl@hotmail.com>
2024-04-15 13:32:34 +0300

    _

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a1413aa81aea6f055846bb7de94af7dd",
-"/": "a1413aa81aea6f055846bb7de94af7dd",
-"main.dart.js": "1f95c19d2135c174635de420548688c1",
+"index.html": "be48f84e1c0d64a5bad8c8c66d8fb3c6",
+"/": "be48f84e1c0d64a5bad8c8c66d8fb3c6",
+"main.dart.js": "9e538a3e0cb228e1537117e05ad7e0d0",
 "manifest.json": "484bcfaee7dda26c8934b6f0a1f7c1c1",
 "version.json": "b3c25d78bb96f4352f7edc20a377d58b"};
 // The application shell files that are downloaded before a service worker can
